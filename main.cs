beff21d841c4322573b62cad48d94f307da7146d
KRYMZ0N <78245990+KRYMZ0N@users.noreply.github.com>
2021-12-17 14:53:22 +0000

    saving stuff to use later

--- main.cs
+++ main.cs
@@ -1,15 +1,27 @@
 using System;
+using System.IO;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("");
+
+    if (File.Exists("config.ucom")) {
+      Console.WriteLine("You are prompted to log in!");
+      Console.Write("User: ");
+    } else {
+      Console.WriteLine("Please register a Username");
+      Console.Write("User: ");
+    }
 
     int i = 1;
     String Input = Console.ReadLine();
 
     while (i > 0) {
-      using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+      using (StreamWriter sw = new StreamWriter("config.ucom"))
         {
+          sw.WriteLine(Input);
+          i = 0;
     }
+  }
   }
 }
