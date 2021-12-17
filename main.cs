d8fb57e434cab21a2489b3f7a909ca3b4f26c90f
KRYMZ0N <78245990+KRYMZ0N@users.noreply.github.com>
2021-12-17 14:41:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+
+    int i = 1;
+    String Input = Console.ReadLine();
+
+    while (i > 0) {
+      using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+        {
+    }
+  }
+}
